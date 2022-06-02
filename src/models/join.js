6395c273d45849f3116aaebacf311c31ddad0ed3
gilang <gilangmohikbal@gmail.com>
2022-06-03 06:32:42 +0700

    create

--- src/models/join.js
+++ src/models/join.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Users, Classes, join}) {
+    static associate({Users, Classes, joins}) {
       joins.belongsTo(Users);
       joins.belongsTo(Classes);
     }
